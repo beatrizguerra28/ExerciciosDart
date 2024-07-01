93d8219c8b815c3ff176b8ae9681bc65a53ce74b
Beatriz Guerra <beatrizg282008@hotmail.com>
2024-07-01 20:48:34 -0300

    questão de número primo

---
+++ Revisão/primo.dart
@@ -0,0 +1,21 @@
+void primo(int numero){
+  int primo = 1;
+
+  for (int i = 2; i < numero; i++) {
+
+    if (numero % 1 == 0){
+      primo = 0;
+
+    }
+  }
+  if (primo == 1){
+    print("O número $numero é primo");
+  } else {
+    print("O número $numero não é primo");
+  }
+}
+
+void main(){
+  primo(2);
+
+}
