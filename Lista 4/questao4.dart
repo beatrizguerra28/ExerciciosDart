6b3cc02b6070c933665afc3df8a619904c2609d4
Beatriz Guerra <beatrizg282008@hotmail.com>
2024-07-02 15:35:34 -0300

    Questão 4

--- Lista 4/questao4.dart
+++ Lista 4/questao4.dart
@@ -1,46 +1,43 @@
-class Livro{
+class Livro {
   String titulo;
   String autor;
   int numeroPaginas;
-  bool estaDisponivel;
-  Livro(this.titulo,this.autor,this.numeroPaginas,){ 
-   estaDisponivel = true;
-}
-  //método emprestar o livro
-  void emprestar(){
-    if(estaDisponivel){
-      estaDisponivel = false;
-      print("O livro '$titulo' foi emprestado.");
-    } else {
-      print("O livro '$titulo' já foi emprestado.");
-  }
-}
-//método devolver o livro
-void devolver(){
-  if(!estaDisponivel){
-    estaDisponivel = true;
-    print("O livro '$titulo' foi devolvido.");
-  } else {
-    print("O livro '$titulo' não foi devolvido.");
+  bool disponibilidade;
 
-    //Método para verificar se o livro está disponível
-    bool estaDisponivel(){
-      return estaDisponivel();
+  Livro(this.titulo, this.autor, this.numeroPaginas) : disponibilidade = true;
+  
+  bool emprestar() { //método emprestar livro
+    if (disponibilidade) {
+      disponibilidade = false;
+      print("Este livro $titulo foi emprestado. Volte em um prazo de 7 dias");
+
+
+    return true;
+     } else {
+    print("O livro $titulo não está disponível para emprestimo");
+    return false;
     }
   }
-
-  void main(){
-    Livro livro1 = Livro("Harry Potter", "J.K. Rowling", 400);
-    Livro livro2 = Livro("O Senhor dos Aneis", "J.R.R. Tolkien", 700);
-    Livro livro3 = Livro("A arte da Guerra", "Sun Tzu", 200);
-    print("O livro '${livro1.titulo}' está disponível? ${livro1.estaDisponivel}");
-    livro1.emprestar();
-    print("O livro '${livro1.titulo}' está disponível? ${livro1.estaDisponivel}");
-    livro1.emprestar();
-    livro1.devolver();
-    print('O livro "${livro1.titulo}" está disponível? ${livro1.estaDisponivel}');
+  bool devolver(){ //método de devolução do livro
+  if (!disponibilidade){
+    disponibilidade = true;
+    print("O livro $titulo foi devolvido");
+    return true;
+  } else {
+    print("O livro $titulo não foi devolvido");
+    return false;
 
   }
+  }
+  bool estaDisponivel() => disponibilidade; // método para verificar se o livro está disponível
+}
 
-
+void main(){
+  Livro livro1 = Livro("Suicidas", "Raphael Montes", 432);
+  // Livro livro2 = Livro("A paciente silente", "Alexandre Michaelides", 350);
+  // print("Disponibilidade Inicial: ${livro1.estaDisponivel()}");
+// print("Disponibilidade Inicial: ${livro2.estaDisponivel()}");
+  livro1.emprestar();
+  livro1.devolver();
+  print("Disponibilidade Inicial: ${livro1.estaDisponivel()}");
 }
